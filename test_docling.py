2ba59debd51918994e722a564d6dc3da84e108a0
ChikkalaSukrutiNaidu <24b01a1222@svecw.edu.in>
2026-05-29 09:57:58 +0530

    Removed unnecessary files

--- test_docling.py
+++
@@ -1,9 +0,0 @@
-from docling.document_converter import DocumentConverter
-
-print("🔥 Docling Started")
-
-converter = DocumentConverter()
-
-result = converter.convert("sample.pdf")
-
-print(result.document.export_to_markdown())
